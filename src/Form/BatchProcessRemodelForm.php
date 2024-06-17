03277c6bbf4cee58c77f22c1b6d307f284d2bdf3
HyphenHook <fghjru1@gmail.com>
2024-06-17 16:32:55 -0400

    Reduce load to 1

--- src/Form/BatchProcessRemodelForm.php
+++ src/Form/BatchProcessRemodelForm.php
@@ -71,7 +71,7 @@ class BatchProcessRemodelForm extends FormBase {
     ->accessCheck(FALSE)
     ->execute();
 
-    $chunks = array_chunk($nids, 2);
+    $chunks = array_chunk($nids, 1);
     $operations = [];
 
     foreach ($chunks as $chunk) {
